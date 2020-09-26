49579a5c12829244b9eb84696a6c8bb98054ab62
meytar <meytarvituri@mail.tau.ac.il>
2020-09-26 16:40:16 +0300

    starting the project

---
+++ build/precache-manifest.52899e829446b0ccec1cdfb221478ab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28191fa4452af5fe257b0c46eba0cc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47bfa83ea3af9a11b432",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ebbb6c5a1326fe3bdedb",
+    "url": "/static/js/2.9f58f1bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f58f1bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47bfa83ea3af9a11b432",
+    "url": "/static/js/main.2825e5be.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
